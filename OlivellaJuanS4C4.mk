626773709d8d9c841e10c283f29433c80eb55152
Juan Diego Olivella Cicero <jd.olivella@uniandes.edu.co>
2018-07-03 11:48:40 -0500

    Carga el MakeFile Corregido

--- OlivellaJuanS4C4.mk
+++ OlivellaJuanS4C4.mk
@@ -1,10 +1,10 @@
-resultados.pdf : resultados.tex
+resultados.pdf : resultados.tex plot.pdf plot1.pdf
 	pdflatex resultados.tex
 
-plot.pdf : plotdatos.py
+plot.pdf : plotdatos.py datos.dat
 	python plotdatos.py
 
-plot1.pdf : plotdatos1.py
+plot1.pdf : plotdatos1.py datos1.dat
 	python plotdatos1.py 
 
 datos.dat : makedatos.py
